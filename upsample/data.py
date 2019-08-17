860848bdcb5a1247917907ea8e043242030f6d26
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2019-08-17 23:36:35 +0900

    added auto in data

--- upsample/data.py
+++ upsample/data.py
@@ -1,23 +1,32 @@
 '''
 provides functions for data io
 '''
+# built-in
 import os
 from functools import partial
+from multiprocessing import cpu_count
 
+# external
 import tensorflow as tf
 
+# original
 from upsample.utils import tfops
+import upsample.utils as utils
 
 class DatasetFactory:
     def __init__(
             self,
             datadir,
-            ncores,
-            ngpus,
+            ncores='auto',
+            ngpus='auto',
     ):
         self.datadir = datadir
-        self.ncores = ncores
-        self.ngpus = ngpus
+
+        if ncores == 'auto': self.ncores = cpu_count()
+        else: self.ncores = ncores
+
+        if ngpus == 'auto': self.ngpus = utils.resource.get_available_gpus()
+        else: self.ngpus = ngpus
         return
 
     def input_train(
@@ -28,12 +37,17 @@ class DatasetFactory:
             batch=True,
             batch_size=5,
             prefetch=True,
-            prefetch_buffer='5000',
+            repeat=True,
+            prefetch_buffer='auto',
+            return_lambda=True,
     ):
         if self.ngpus == 0:
             actual_batch_size = batch_size
         else:
             actual_batch_size = batch_size // self.ngpus
+
+        if prefetch_buffer == 'auto':
+            prefetch_buffer = tf.data.experimental.AUTOTUNE
 
         dataset = self.dataset_list(mode='train')
         dataset = self.decode(dataset)
@@ -41,7 +55,12 @@ class DatasetFactory:
         dataset = dataset.shuffle(shuffle_buffer)
         dataset = dataset.batch(actual_batch_size)
         dataset = dataset.prefetch(prefetch_buffer)
-        return dataset
+
+        if repeat:
+            dataset = dataset.repeat()
+
+        if return_lambda: return lambda: dataset
+        else: return dataset
 
     def input_eval(
             self,
@@ -49,14 +68,20 @@ class DatasetFactory:
             batch=True,
             batch_size=5,
             prefetch=True,
-            prefetch_buffer='5000',
+            prefetch_buffer='auto',
+            return_lambda=True,
     ):
+        if prefetch_buffer == 'auto':
+            prefetch_buffer = tf.data.experimental.AUTOTUNE
+
         dataset = self.dataset_list(mode='eval')
         dataset = self.decode(dataset)
         dataset = self.add_downsampled(dataset, method=downsample_method)
         dataset = dataset.batch(batch_size)
         dataset = dataset.prefetch(prefetch_buffer)
-        return dataset
+
+        if return_lambda: return lambda: dataset
+        else: return dataset
 
     def input_predict(
             self,
