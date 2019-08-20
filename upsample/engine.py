679661acf1ea0ac8e0e73165f952ddf6c9f50937
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2019-08-20 23:25:17 +0900

    minor bug fixes

--- upsample/engine.py
+++ upsample/engine.py
@@ -53,7 +53,7 @@ def get_estimator(
             this param will be overwritten by it.
     """
     if assign_gpu == 'auto':
-        assign_gpu = utils.resources.get_available_gpus()
+        assign_gpu = utils.resource.get_available_gpus()
 
     config_session = tf.ConfigProto(
         # inter_op_parallelism_threads=tio.FLAGS.cores,
--- upsample/utils/__init__.py
+++ upsample/utils/__init__.py
@@ -1,1 +1,1 @@
-from upsample.utils import tfops, hash, param, resource, result, loggings, resource
+from upsample.utils import tfops, hash, param, resource, result, loggings
